49b5af82c3ea1083ae754a7d91e1360316144238
Colin Campbell-McPherson <colin@logaan.net>
2009-07-26 02:35:25 +0800

    added test assertion

--- test/test_hello_world.rb
+++ test/test_hello_world.rb
@@ -5,6 +5,7 @@ class HelloWorldTest < Test::Unit::TestCase
   def test_parser
     examples_dir = File.join( File.dirname( __FILE__ ), '..', 'examples' )
     doc = Unot.new( examples_dir , 'hello_world' )
+    assert( true, doc.respond_to?( :insert_greeting ) )
   end
 end
 
